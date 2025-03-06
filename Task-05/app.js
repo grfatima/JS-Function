45c69d52fc746430afae7466762b2c00e5f74116
grfatime <kurbanovhafatime@gmail.com>
2025-03-07 02:37:25 +0400

    completed

--- Task-05/app.js
+++
@@ -1,9 +0,0 @@
-function isLucky(n1, n2, n3, n4, n5, n6) {
-  if (n1 + n2 + n3 == n4 + n5 + n6) {
-    return true;
-  } else {
-    return false;
-  }
-}
-const result = isLucky(2, 2, 2, 4, 1, 1);
-console.log(result);
---
+++ Task-06/app.js
@@ -0,0 +1,9 @@
+function isLucky(n1, n2, n3, n4, n5, n6) {
+    if (n1 + n2 + n3 == n4 + n5 + n6) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  const result = isLucky(2, 2, 2, 4, 1, 1);
+  console.log(result);
